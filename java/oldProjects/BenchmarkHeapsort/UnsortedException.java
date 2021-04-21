3f31356ac9f26289c418e68eef74428533fcd744
Jacob <jcbroe19@gmail.com>
2021-04-21 09:39:43 -0400

    Update UnsortedException.java

--- java/oldProjects/BenchmarkHeapsort/UnsortedException.java
+++ java/oldProjects/BenchmarkHeapsort/UnsortedException.java
@@ -1,10 +1,3 @@
-/*
- * Name: Jacob Roe
- * CMSC 451
- * Project: 1
- * This is the UnsortedException class. It's pretty super.
- */
-
 
 public class UnsortedException extends Exception {
     public UnsortedException() { super(); }
