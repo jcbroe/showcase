df06954fb0342854f3ab104d3da3f407a4b3824f
Jacob <jcbroe19@gmail.com>
2021-04-21 09:39:04 -0400

    Update SortInterface.java

--- java/oldProjects/BenchmarkHeapsort/SortInterface.java
+++ java/oldProjects/BenchmarkHeapsort/SortInterface.java
@@ -1,11 +1,3 @@
-/*
- * Name: Jacob Roe
- * CMSC 451
- * Project: 1
- * This is the interface class.
- */
-
-
 public interface SortInterface {
     void recursiveSort(int[] list) throws UnsortedException;
     void iterativeSort(int[] list) throws UnsortedException;
