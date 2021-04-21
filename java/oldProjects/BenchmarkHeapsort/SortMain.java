f5d691994bec08b6f8d52dbae7810b6161623fe1
Jacob <jcbroe19@gmail.com>
2021-04-21 09:39:25 -0400

    Update SortMain.java

--- java/oldProjects/BenchmarkHeapsort/SortMain.java
+++ java/oldProjects/BenchmarkHeapsort/SortMain.java
@@ -1,10 +1,3 @@
-/*
- * Name: Jacob Roe
- * CMSC 451
- * Project: 1
- * This is the main driver for the project.
- */
-
 public class SortMain {
     public static void main(String[] args) {
         int[] testSizes = {10, 20, 50, 100, 200,
