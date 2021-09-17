e15f70041862c6b729d9d9ffcbf9f91d68b25f06
Jacob <jcbroe19@gmail.com>
2021-09-17 18:50:12 -0400

    Create Problem005.cpp

---
+++ Project Euler/C++/Problem005.cpp
@@ -0,0 +1,29 @@
+/*
+* 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+* What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int gcd(int a, int b)
+{
+	if (b == 0)
+	{
+		return a;
+	}
+	return gcd(b, a % b);
+}
+
+int main()
+{
+	//start at 1 so that multiplication works
+	int total = 1;
+	for (int i = 1; i < 21; i++) //1 to 20
+	{
+		total *= i / gcd(i, total);
+	}
+
+	cout << total;
+}
